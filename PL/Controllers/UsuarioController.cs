6d8151f6cf3e7a4470542889b8dbb4283a8e5e6c
Christian Rogelio Hernandez Padilla <chrisrhernandezp@gmail.com>
2025-07-04 18:33:04 -0600

    Avance Controlador

--- PL/Controllers/UsuarioController.cs
+++ PL/Controllers/UsuarioController.cs
@@ -26,7 +26,7 @@ namespace PL.Controllers
                 ML.Result result = BL.Usuario.GetById(IdUsuario.Value);
                 if(result.Correct == true)
                 {
-
+                    usuario = (ML.Usuario)result.Object;
                 }
             }
             return View(usuario);
@@ -34,7 +34,21 @@ namespace PL.Controllers
         [HttpPost]
         public ActionResult Formulario(ML.Usuario usuario)
         {
-            
+            ML.Result result = new ML.Result();
+            if(usuario.IdUsuario == 0)
+            {
+                result = BL.Usuario.Add(usuario);
+
+                if(result.Correct == true)
+                {
+                    TempData["Success"] = "Usuario Agregado Correctamente";
+                    return RedirectToAction("GetAll");
+                }
+                else
+                {
+                    TempData["Error"] = "Error al agregar el Usuario" + result.ErrorMessage;
+                }
+            }
             return View(usuario);
         }
         [HttpDelete]
