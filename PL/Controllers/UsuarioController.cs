ae5be1192164b628798fde4aae2534088d0a06f2
Christian Rogelio Hernandez Padilla <chrisrhernandezp@gmail.com>
2025-07-04 18:24:36 -0600

    PL mas el inicio del contralodor

--- PL/Controllers/UsuarioController.cs
+++ PL/Controllers/UsuarioController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ML;
+using System;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +11,37 @@ namespace PL.Controllers
     public class UsuarioController : Controller
     {
         // GET: Usuario
+        [HttpGet]
         public ActionResult GetAll()
         {
-
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Formulario(int? IdUsuario) 
+        {
+            ML.Usuario usuario = new ML.Usuario();
+            if(IdUsuario != null && IdUsuario > 0)
+            {
+                ML.Result result = BL.Usuario.GetById(IdUsuario.Value);
+                if(result.Correct == true)
+                {
+
+                }
+            }
+            return View(usuario);
+        }
+        [HttpPost]
+        public ActionResult Formulario(ML.Usuario usuario)
+        {
+            
+            return View(usuario);
+        }
+        [HttpDelete]
+        public ActionResult Delete(int IdUsuario) 
+        {
+
+            return RedirectToAction("GetAll");
         }
     }
 }
