3768bb787b1beb912de6155840fd0572d33a5aaa
Leonardo Blancas Uribe <leonardobu28@outlook.com>
2025-07-07 15:53:49 -0600

    Detalles

--- BL/Usuario.cs
+++ BL/Usuario.cs
@@ -154,7 +154,7 @@ namespace BL
                         usuario.Nombre = usuariobyid.Nombre;
                         usuario.Apellido = usuariobyid.Apellido;
                         usuario.FechaNacimiento = (usuariobyid.FechaNacimiento).Value.ToString("yyyy-MM-dd");
-                        result.Object = usuario;
+                        
                         result.Correct = true;
                     }
                     else
